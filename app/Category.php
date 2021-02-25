628eefcf2b21a5024d2a27e4211c3d95907463fb
egitriyanaf <egitriyanafidollah@gmail.com>
2021-02-25 07:07:44 +0700

    management category

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    public function parent(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeGetParent($query){
+        return $query->whereNull('parent_id');
+    }
+
+    protected $fillable = ['name', 'parent_id', 'slug'];
+
+    public function setSlugAttribute($value){
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getNameAttributes($value){
+        return ucfirst($value);
+    }
+
+    public function child(){
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
